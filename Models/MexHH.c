0aec815ee1e824d286cca13b06963e504d0e1613
Unknown <johnwalmsley@gmail.com>
2018-07-11 13:59:14 +0100

    Updates re-included
    This commit returns the models to their current versions, so all changes can be seen to the code since Kylie's original version in the sine-wave repository.

--- Models/MexHH.c
+++ Models/MexHH.c
@@ -45,8 +45,8 @@ static void MexHHScaled(N_Vector ydot, realtype t, N_Vector y, double* pr, doubl
     
     NV_Ith_S(abstol, 0) = 1e-8;
     NV_Ith_S(abstol, 1) = 1e-8;
-    NV_Ith_S(abstol, 2) = 1e-8;
-    realtype reltol = 1e-8;
+        NV_Ith_S(abstol, 2) = 1e-8;
+      realtype reltol = 1e-8;
     
     /* Set up CVode*/
     int flag, k;
@@ -56,7 +56,7 @@ static void MexHHScaled(N_Vector ydot, realtype t, N_Vector y, double* pr, doubl
     NV_Ith_S(y0, 0) = Y0[0];
     NV_Ith_S(y0, 1) = Y0[1];
     NV_Ith_S(y0, 2) = Y0[2];
-    
+
     realtype NState = 3;
     
     /* Solver options*/
@@ -71,16 +71,16 @@ static void MexHHScaled(N_Vector ydot, realtype t, N_Vector y, double* pr, doubl
     flag = CVodeSVtolerances(cvode_mem, reltol, abstol);
     flag = CVDense(cvode_mem, NState);
     flag = CVodeSetMaxStep(cvode_mem, 0.1);
-    
+
     /* Call CVode test for error and add result to output vector yout */
     for (k = 1; k < N; ++k) {
         double tout = (k*(Tfinal)/N);
-        
-        
+   
+
         if (CVode(cvode_mem, tout, y0, &t, CV_NORMAL) < 0) {
             fprintf(stderr, "Error in CVode: %d\n", flag);
         }
-        
+      
         /*Probability of being in open state is equal to 1-probability of being in any other state*/
         yout[k] = NV_Ith_S(y0, 2);
         
@@ -96,123 +96,530 @@ static void MexHHScaled(N_Vector ydot, realtype t, N_Vector y, double* pr, doubl
 static int f(realtype t, N_Vector y, N_Vector ydot, void* pr0) {
     double* pr = pr0;
     
-    /* Define voltage protocol to be used*/
+   /* Define voltage protocol to be used*/
     double v;
-    /* This shift is needed for simulated protocol to match the protocol recorded in experiment, which is shifted by 0.1ms as compared to the original input protocol. Consequently, each step is held for 0.1ms longer in this version of the protocol as compared to the input.*/
-    double shift = 0.1;
+   /* This shift is needed for simulated protocol to match the protocol recorded in experiment, which is shifted by 0.1ms as compared to the original input protocol. Consequently, each step is held for 0.1ms longer in this version of the protocol as compared to the input.*/
+   double shift = 0.1;
     /* The number corresponding to the protocol to be simulated is passed at the front of the parameter values vector*/
     double protocol_number = pr[0];
     
-    
-    /* sine wave*/
+ 
+             /* made_up_1_shifted*/
     if (protocol_number==1) {
         double C[6] = {54, 26, 10, 0.007/(2*M_PI), 0.037/(2*M_PI), 0.19/(2*M_PI)};
-        
-        if (t>=0 && t<250+shift)
-        {
-            v = -80;
-        }
-        
-        if (t>=250+shift && t<300+shift)
-        {
-            v = -120;
-        }
-        
-        if (t>=300+shift && t<500+shift)
-        {
-            v = -80;
-        }
-        
-        if (t>=500+shift && t<1500+shift)
-        {
-            v = 40;
-        }
-        
-        
-        if (t>=1500+shift && t<2000+shift)
-        {
-            v = -120;
-        }
-        if (t>=2000+shift && t<3000+shift)
-        {
-            v = -80;
-        }
-        
-        
-        if (t>=3000+shift && t<6500+shift)
-        {
-            v=-30+C[0]*(sin(2*M_PI*C[3]*(t-2500-shift))) + C[1]*(sin(2*M_PI*C[4]*(t-2500-shift))) + C[2]*(sin(2*M_PI*C[5]*(t-2500-shift)));
-            
-        }
-        if (t>=6500+shift && t<7000+shift)
-        {
-            v=-120;
-        }
-        if (t>= 7000+shift && t<8000+shift)
-        {
-            v = -80;
-        }
-    }
+     
+         if (t>=0 && t<250+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=250+shift && t<300+shift)
+   {
+       v = -120;
+   }
+   
+   if (t>=300+shift && t<500+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=500+shift && t<1500+shift)
+   {
+       v = 40;
+   }  
+   
+   
+   if (t>=1500+shift && t<2000+shift)
+   {
+       v = -120;
+   }
+   if (t>=2000+shift && t<3000+shift)
+   {
+       v = -80;
+   }
     
-    
-    if (protocol_number==7)
+
+    if (t>=3000+shift && t<6500+shift)
     {
-        
-        int l = floor(10*t);
-        v=pr[l+9];
-        
+           v=-30+C[0]*(sin(2*M_PI*C[3]*(t-2500-shift))) + C[1]*(sin(2*M_PI*C[4]*(t-2500-shift))) + C[2]*(sin(2*M_PI*C[5]*(t-2500-shift))); 
+
+    }
+if (t>=6500+shift && t<7000+shift)
+{
+    v=-120;
+}
+    if (t>= 7000+shift && t<8000+shift)
+    {
+        v = -80;
+    }
     }
     
+           /* made_up_2_shifted*/
+    if (protocol_number==2) {
+        double C[6] = {45, -32, -14, 0.00357/(2*M_PI), 0.057/(2*M_PI), 0.119/(2*M_PI)};
+        
+  
+              if (t>=0 && t<250+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=250+shift && t<300+shift)
+   {
+       v = -120;
+   }
+   
+   if (t>=300+shift && t<500+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=500+shift && t<1500+shift)
+   {
+       v = 40;
+   }  
+   
+   
+   if (t>=1500+shift && t<2000+shift)
+   {
+       v = -120;
+   }
+   if (t>=2000+shift && t<3000+shift)
+   {
+       v = -80;
+   }
+    
+
+    if (t>=3000+shift && t<6500+shift)
+    {
+           v=-30+C[0]*(sin(2*M_PI*C[3]*(t-2500-shift))) + C[1]*(sin(2*M_PI*C[4]*(t-2500-shift))) + C[2]*(sin(2*M_PI*C[5]*(t-2500-shift))); 
+
+    }
+if (t>=6500+shift && t<7000+shift)
+{
+    v=-120;
+}
+    if (t>= 7000+shift && t<8000+shift)
+    {
+        v = -80;
+    }
+    }
+  
+     
+           /* made_up_3_shifted*/
+    if (protocol_number==3) {
+        double C[6] = {50, 28, -11, 0.03/(2*M_PI), 0.123/(2*M_PI), 0.199/(2*M_PI)};
+        
+  
+            if (t>=0 && t<250+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=250+shift && t<300+shift)
+   {
+       v = -120;
+   }
+   
+   if (t>=300+shift && t<500+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=500+shift && t<1500+shift)
+   {
+       v = 40;
+   }  
+   
+   
+   if (t>=1500+shift && t<2000+shift)
+   {
+       v = -120;
+   }
+   if (t>=2000+shift && t<3000+shift)
+   {
+       v = -80;
+   }
+    
+
+    if (t>=3000+shift && t<6500+shift)
+    {
+         v=-30+C[0]*(sin(2*M_PI*C[3]*(t-2500-shift))) + C[1]*(sin(2*M_PI*C[4]*(t-2500-shift))) + C[2]*(sin(2*M_PI*C[5]*(t-2500-shift))); 
+
+    }
+if (t>=6500+shift && t<7000+shift)
+{
+    v=-120;
+}
+    if (t>= 7000+shift && t<8000+shift)
+    {
+        v = -80;
+    }
+    }
+          
+          
+    
+        /* wang eq prop*/
+    if (protocol_number==4) {
+        double C[6] = {19.4704157,29.27670088,	-43.51162825,	0.000413039,	0.001402155,	0.010001046};
+        
+  
+              if (t>=0 && t<250+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=250+shift && t<300+shift)
+   {
+       v = -120;
+   }
+   
+   if (t>=300+shift && t<500+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=500+shift && t<1500+shift)
+   {
+       v = 40;
+   }  
+   
+   
+   if (t>=1500+shift && t<2000+shift)
+   {
+       v = -120;
+   }
+   if (t>=2000+shift && t<3000+shift)
+   {
+       v = -80;
+   }
+    
+
+    if (t>=3000+shift && t<6500+shift)
+    {
+          v=-30+C[0]*(sin(2*M_PI*C[3]*(t-2500-shift))) + C[1]*(sin(2*M_PI*C[4]*(t-2500-shift))) + C[2]*(sin(2*M_PI*C[5]*(t-2500-shift))); 
+
+    }
+if (t>=6500+shift && t<7000+shift)
+{
+    v=-120;
+}
+    if (t>= 7000+shift && t<8000+shift)
+    {
+        v = -80;
+    }
+    }
+      
+    
+        /* diveroli eq prop*/
+    if (protocol_number==5) {
+        double C[6] = {52.2240105,	-18.9443991,	-38.51567768,	0.00093839,	0.001000016,	0.016839351};
+        
+  
+              if (t>=0 && t<250+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=250+shift && t<300+shift)
+   {
+       v = -120;
+   }
+   
+   if (t>=300+shift && t<500+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=500+shift && t<1500+shift)
+   {
+       v = 40;
+   }  
+   
+   
+   if (t>=1500+shift && t<2000+shift)
+   {
+       v = -120;
+   }
+   if (t>=2000+shift && t<3000+shift)
+   {
+       v = -80;
+   }
+    
+
+    if (t>=3000+shift && t<6500+shift)
+    {
+          v=-30+C[0]*(sin(2*M_PI*C[3]*(t-2500-shift))) + C[1]*(sin(2*M_PI*C[4]*(t-2500-shift))) + C[2]*(sin(2*M_PI*C[5]*(t-2500-shift))); 
+
+    }
+if (t>=6500+shift && t<7000+shift)
+{
+    v=-120;
+}
+    if (t>= 7000+shift && t<8000+shift)
+    {
+        v = -80;
+    }
+    }
+    
+    
+      /* max diff*/
+    if (protocol_number==6) {
+        double C[6] = {53.09022186,	-27.35887966,	10.69044491,	0.000456713,	0.001055862,	0.028866675};
+        
+  
+              if (t>=0 && t<250+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=250+shift && t<300+shift)
+   {
+       v = -120;
+   }
+   
+   if (t>=300+shift && t<500+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=500+shift && t<1500+shift)
+   {
+       v = 40;
+   }  
+   
+   
+   if (t>=1500+shift && t<2000+shift)
+   {
+       v = -120;
+   }
+   if (t>=2000+shift && t<3000+shift)
+   {
+       v = -80;
+   }
+    
+
+    if (t>=3000+shift && t<6500+shift)
+    {
+     v=-30+C[0]*(sin(2*M_PI*C[3]*(t-2500-shift))) + C[1]*(sin(2*M_PI*C[4]*(t-2500-shift))) + C[2]*(sin(2*M_PI*C[5]*(t-2500-shift))); 
+
+    }
+if (t>=6500+shift && t<7000+shift)
+{
+    v=-120;
+}
+    if (t>= 7000+shift && t<8000+shift)
+    {
+        v = -80;
+    }
+    }
+
+
+
+
+  /* original_sine*/
+    if (protocol_number==15) {
+        double C[6] = {57, 18, 28, 0.031, 0.112, 0.08};
+     
+         if (t>=0 && t<250+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=250+shift && t<300+shift)
+   {
+       v = -120;
+   }
+   
+   if (t>=300+shift && t<500+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=500+shift && t<1500+shift)
+   {
+       v = 40;
+   }  
+   
+   
+   if (t>=1500+shift && t<2000+shift)
+   {
+       v = -120;
+   }
+   if (t>=2000+shift && t<3000+shift)
+   {
+       v = -80;
+   }
+    
+
+    if (t>=3000+shift && t<8000+shift)
+    {
+           v=-30+C[0]*(sin(2*M_PI*C[3]*(t-2900-shift))) + C[1]*(sin(2*M_PI*C[4]*(t-2900-shift))) + C[2]*(sin(2*M_PI*C[5]*(t-2900-shift))); 
+
+    }
+if (t>=8000+shift && t<8500+shift)
+{
+    v=-120;
+}
+    if (t>= 8500+shift && t<9500+shift)
+    {
+        v = -80;
+    }
+    }
+  /* equal_proportions*/
+    if (protocol_number==16) {
+        double C[6] = {51, 71, 17, -8e-5, 0.004, 0.005};
+     
+         if (t>=0 && t<250+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=250+shift && t<300+shift)
+   {
+       v = -120;
+   }
+   
+   if (t>=300+shift && t<500+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=500+shift && t<1500+shift)
+   {
+       v = 40;
+   }  
+   
+   
+   if (t>=1500+shift && t<2000+shift)
+   {
+       v = -120;
+   }
+   if (t>=2000+shift && t<3000+shift)
+   {
+       v = -80;
+   }
+    
+
+    if (t>=3000+shift && t<8000+shift)
+    {
+           v=-30+C[0]*(sin(2*M_PI*C[3]*(t-2900-shift))) + C[1]*(sin(2*M_PI*C[4]*(t-2900-shift))) + C[2]*(sin(2*M_PI*C[5]*(t-2900-shift))); 
+
+    }
+if (t>=8000+shift && t<8500+shift)
+{
+    v=-120;
+}
+    if (t>= 8500+shift && t<9500+shift)
+    {
+        v = -80;
+    }
+    }
+
+
+  /* maz_wang_div_diff*/
+    if (protocol_number==17) {
+        double C[6] = {25.3, 99.7, 4.2, -0.00007, 0.003, -0.16};
+     
+         if (t>=0 && t<250+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=250+shift && t<300+shift)
+   {
+       v = -120;
+   }
+   
+   if (t>=300+shift && t<500+shift)
+   {
+       v = -80;
+   }
+   
+   if (t>=500+shift && t<1500+shift)
+   {
+       v = 40;
+   }  
+   
+   
+   if (t>=1500+shift && t<2000+shift)
+   {
+       v = -120;
+   }
+   if (t>=2000+shift && t<3000+shift)
+   {
+       v = -80;
+   }
+    
+
+    if (t>=3000+shift && t<8000+shift)
+    {
+           v=-30+C[0]*(sin(2*M_PI*C[3]*(t-2900-shift))) + C[1]*(sin(2*M_PI*C[4]*(t-2900-shift))) + C[2]*(sin(2*M_PI*C[5]*(t-2900-shift))); 
+
+    }
+if (t>=8000+shift && t<8500+shift)
+{
+    v=-120;
+}
+    if (t>= 8500+shift && t<9500+shift)
+    {
+        v = -80;
+    }
+    }
+
+
+
+    if (protocol_number==7)
+{
+    
+int l = floor(10*t);
+v=pr[l+9];
+
+} 
+       if (protocol_number==8){
+    
+int l = floor(10*t);
+v=pr[l+9];
+
+} 
     if (protocol_number==9){
-        
-        int l = floor(10*t);
-        v=pr[l+9];
-        
-    }
     
-    if (protocol_number==11){
-        
-        int l = floor(10*t);
-        v=pr[l+9];
-        
-    }
-    if (protocol_number==12){
-        
-        int l = floor(10*t);
-        v=pr[l+9];
-        
-    }
-    if (protocol_number==13){
-        
-        int l = floor(10*t);
-        v=pr[l+9];
-        
-    }     if (protocol_number==14){
-        
-        int l = floor(10*t);
-        v=pr[l+9];
-        
-    }
-    if (protocol_number==18){
-        
-        int l = floor(10*t);
-        v=pr[l+9];
-        
-    }     if (protocol_number==19){
-        
-        int l = floor(10*t);
-        v=pr[l+9];
-        
-    }
-    if (protocol_number==20){
-        
-        int l = floor(10*t);
-        v=pr[l+9];
-        
-    }
+int l = floor(10*t);
+v=pr[l+9];
+
+}    
+    if (protocol_number==10){
+    
+int l = floor(10*t);
+v=pr[l+9];
+
+} 
+        if (protocol_number==11){
+    
+int l = floor(10*t);
+v=pr[l+9];
+
+} 
+        if (protocol_number==12){
+    
+int l = floor(10*t);
+v=pr[l+9];
+
+} 
+        if (protocol_number==13){
+    
+int l = floor(10*t);
+v=pr[l+9];
+
+}     if (protocol_number==14){
+    
+int l = floor(10*t);
+v=pr[l+9];
+
+} 
     /* Parameters from input parameters - as the first element of the vector corresponds to the protocol number, the parameter values start from the second element*/
     
-    
+   
     double P0 = pr[1];
     double P1 = pr[2];
     double P2 = pr[3];
@@ -221,38 +628,38 @@ static int f(realtype t, N_Vector y, N_Vector ydot, void* pr0) {
     double P5 = pr[6];
     double P6 = pr[7];
     double P7 = pr[8];
-    
-    
+
+
     
     /*Ensures microscopic reversibility condition satisfied*/
-    
+  
     const double y1 = NV_Ith_S(y, 0);
     const double y2 = NV_Ith_S(y, 1);
     const double y3 = NV_Ith_S(y, 2);
-    
-    const double y4 = (1.0-y1-y2-y3);
+
+    const double y4 = (1.0-y1-y2-y3); 
     /* Model equations*/
     
-    
-    
-    const double k32 = P4*exp(P5*v);
-    const double k23 = P6*exp(-P7*v);
-    
-    const double k43 = P0*exp(P1*v);
-    const double k34 = P2*exp(-P3*v);
-    
-    const double k12 = k43;
-    const double k21 = k34;
-    
-    const double k41 = k32;
-    const double k14 = k23;
-    
-    
-    NV_Ith_S(ydot, 0) = -k12*y1 + k21*y2 + k41*y4 - k14*y1;
-    NV_Ith_S(ydot, 1) = -k23*y2 + k32*y3 + k12*y1 - k21*y2;
-    NV_Ith_S(ydot, 2) = -k34*y3 + k43*y4 + k23*y2 - k32*y3;
-    
-    
+	
+	
+	        const double k32 = P4*exp(P5*v);
+	        const double k23 = P6*exp(-P7*v);
+
+	        const double k43 = P0*exp(P1*v);
+	        const double k34 = P2*exp(-P3*v);
+	
+	        const double k12 = k43;
+	        const double k21 = k34;
+
+ 		const double k41 = k32;
+	        const double k14 = k23;
+
+
+	        NV_Ith_S(ydot, 0) = -k12*y1 + k21*y2 + k41*y4 - k14*y1;
+	        NV_Ith_S(ydot, 1) = -k23*y2 + k32*y3 + k12*y1 - k21*y2;
+	        NV_Ith_S(ydot, 2) = -k34*y3 + k43*y4 + k23*y2 - k32*y3;
+	   
+ 
     return 0;
 }
 
