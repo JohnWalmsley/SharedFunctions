01e8200f99fbe18437b5d66a280e1e735ddf106c
Unknown <johnwalmsley@gmail.com>
2018-07-30 13:32:12 +0100

    Updated FindingBestFits to work with either cell or string
    This revision removes the need for a separate FindingBestFitsAfterMCMC in DiscrepancyCorrelations

--- FindingBestFitsAfterMCMC.m
+++ FindingBestFitsAfterMCMC.m
@@ -1,4 +1,4 @@
-function [P,V] = FindingBestFitsAfterMCMC(model,protocol,exp_ref)
+function [P,V] = FindingBestFitsAfterMCMC(model,protocols,exp_ref)
 % Function imports MCMC chains and likelihood values for relevant model/protocol/exp_ref combination
 cd  ../MCMCResults
 pathToFolder = '.';
@@ -6,9 +6,19 @@ files = dir(fullfile(pathToFolder,'*'));
 
 P=[];
 V=[];
+
+if isa( protocols, 'cell' )
+    fitting_protocol = protocols{ 1 };
+    for pr = 2 : length( protocols )
+        fitting_protocol = [ fitting_protocol '_' protocols{ pr } ];
+    end
+else
+    fitting_protocol = protocols;
+end
+
 for i = 1:numel(files)
     
-    k = regexp(files(i).name,['MCMCLikelihood_',exp_ref,'_',model,'_',protocol ]);
+    k = regexp(files(i).name,['MCMCLikelihood_',exp_ref,'_',model,'_',fitting_protocol ]);
 
     if k>=1
         V=[V;importdata(files(i).name)];
